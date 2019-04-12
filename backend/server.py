6e5604e929afcc5be0d56bc412d70db30521d99d
szymski <szymskipl@gmail.com>
2019-04-12 18:32:34 +0200

    Radio playback

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,7 @@ from flask_uploads import UploadSet, configure_uploads, AUDIO
 from flask_cors import CORS
 from tinytag import TinyTag
 import subprocess
+import signal
 import datetime
 import os
 
@@ -16,13 +17,26 @@ app.config["UPLOADED_MUSIC_DEST"] = "static/audio"
 app.config["JSON_AS_ASCII"] = False
 configure_uploads(app, music)
 
+pifm_proc = None
+
 @app.route('/start', methods=["POST"])
 def start():
+    global pifm_proc
     file_name = request.form["file_name"]
     freq = request.form["freq"]
     radio_text = request.form["radio_text"]
-    m = subprocess.Popen("./pi_fm_adv --audio " + file_name + " --freq " + freq + " --rt " + radio_text)
-    m.wait()
+    # m = subprocess.Popen("./pi_fm_adv --audio " + file_name + " --freq " + freq + " --rt " + radio_text)
+    # m.wait()
+    if pifm_proc and not pifm_proc.poll():
+        print("Killing")
+        # os.killpg(os.getpgid(pifm_proc.pid), signal.SIGTERM)
+        subprocess.Popen("sudo killall pi_fm_adv", shell=True)
+        print("Killed")
+        pifm_proc = None
+
+    cmd = "sox -t mp3 {} -t wav - | sudo ./pi_fm_adv --audio - --freq {} --rt {}".format(file_name, freq, radio_text) 
+    print("Cmd: {}".format(cmd))
+    pifm_proc = subprocess.Popen(cmd, shell=True, cwd="static/audio", preexec_fn=os.setsid)
     return jsonify(), 200
 
 @app.route('/sysinfo')
@@ -32,27 +46,33 @@ def sysinfo():
 @app.route('/ls')
 def file_list():
     payload = {}
-    p = subprocess.Popen("ls", stdout=subprocess.PIPE, shell=True, cwd="static/audio")
-    p.wait()
-    files = p.stdout.readlines()
-    files = [x.strip() for x in files]
+    # p = subprocess.Popen("ls", stdout=subprocess.PIPE, shell=True, cwd="static/audio")
+    # p.wait()
+    # files = p.stdout.readlines()
+    # files = [x.strip() for x in files]
     i = 0
-    for file in files:
-        f = TinyTag.get("static/audio/" + file, image=True)
-        if not f.title:
-            f.title = file
-        img_exists = os.path.isfile("static/img/" + file.split(".")[0] + ".png")
-        if img_exists:
-            img_path = file.split(".")[0] + ".png"
-        else:
-            img_path = None
-        payload[i] = {
-            "name": f.title,
-            "length": f.duration,
-            "author": f.artist,
-            "img": img_path
-        }
-        i += 1
+    for r, d, f in os.walk("static/audio"):
+        for f2 in f:
+            try:
+                file = f2
+                f = TinyTag.get("static/audio/" + file, image=True)
+                if not f.title:
+                    f.title = file
+                img_exists = os.path.isfile("static/img/" + file.split(".")[0] + ".png")
+                if img_exists:
+                    img_path = file.split(".")[0] + ".png"
+                else:
+                    img_path = None
+                payload[i] = {
+                    "filename": file,
+                    "name": f.title,
+                    "length": f.duration,
+                    "author": f.artist,
+                    "img": img_path
+                }
+                i += 1
+            except:
+                pass
     return jsonify(payload)
     
 @app.route('/upload', methods=["POST"])
@@ -69,3 +89,6 @@ def upload():
 
 if __name__ == "__main__":
     app.run(port=9000, host='0.0.0.0')
+
+# p = subprocess.Popen("sox -t mp3 nighoffire.mp3 -t wav - | sudo ./pi_fm_adv --audio - --freq 88", stdout=subprocess.PIPE, shell=True, cwd="static/audio")
+# p.wait() 
